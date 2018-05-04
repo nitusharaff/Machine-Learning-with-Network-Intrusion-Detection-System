bfc942a74ada4fc8cb3e4b5b563f94349f4d49c1
nitusharaff <35472902+nitusharaff@users.noreply.github.com>
2018-05-03 19:10:18 -0700

    Update preprocess.R

--- preprocess.R
+++ preprocess.R
@@ -1,6 +1,6 @@
 library(mlbench)
 library(FSelector)
-train= read.csv("D:\\sem 6\\pattern classification\\kddtrain.csv" )
+train= read.csv("kddtrain.csv" )
 sapply(train$Protocol_type, function(x) length(unique(x)))
 
 train$Protocol_type= as.numeric(train$Protocol_type)
@@ -8,9 +8,9 @@ train$Protocol_type= as.numeric(train$Protocol_type)
 train$Service= as.numeric(train$Service)
 train$Flag= as.numeric(train$Flag)
 train$Attacks= as.numeric(train$Attacks)
-write.csv(train, "D:\\sem 6\\pattern classification\\kddtrain.csv", row.names=FALSE)
+write.csv(train, "kddtrain.csv", row.names=FALSE)
 
-train= read.csv("D:\\sem 6\\pattern classification\\kddtest.csv" )
+train= read.csv("kddtest.csv" )
 sapply(train$Protocol_type, function(x) length(unique(x)))
 
 train$Protocol_type= as.numeric(train$Protocol_type)
@@ -18,5 +18,5 @@ train$Protocol_type= as.numeric(train$Protocol_type)
 train$Service= as.numeric(train$Service)
 train$Flag= as.numeric(train$Flag)
 train$Attacks= as.numeric(train$Attacks)
-write.csv(train, "D:\\sem 6\\pattern classification\\kddtest.csv", row.names=FALSE)
+write.csv(train, "kddtest.csv", row.names=FALSE)
 
