93081e5e42ad6564988d3a6ef77c53510888011e
nitusharaff <35472902+nitusharaff@users.noreply.github.com>
2018-05-03 19:11:25 -0700

    Update chisq.R

--- chisq.R
+++ chisq.R
@@ -3,11 +3,10 @@ library(FSelector)
 
  library(rpart)
  data(train1, package="mlbench")
- ## split data into a train and test set
-train= read.csv("D:\\sem 6\\pattern classification\\nslkddtrain.csv" )
 
-#train= read.csv("D:\\san fransisco\\train.csv" )
-#data(train)
+train= read.csv("nslkddtrain.csv" )
+
+
 weights <- chi.squared(Attacks~., train)
 print(weights)
 subset <- cutoff.k(weights, 25)
